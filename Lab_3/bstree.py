be59464c2a04f096ef69cddd617f7c663950d676
Roman Prokopchuk <ghost62727@gmail.com>
2023-04-11 18:10:05 +0300

    Add raw version

---
+++ Lab_3/bstree.py
@@ -0,0 +1,128 @@
+class Node:
+    def __init__(self, value, left=None, right=None, parent=None):
+        self.value = value
+        self.left = left
+        self.right = right
+        self.parent = parent
+
+    def is_leaf(self):
+        return self.left is None and self.right is None
+
+
+class BSTree:
+    def __init__(self):
+        self.root = None
+
+    def insert(self, value, key):
+        to_insert = Node(value)
+        current = self.root
+        pos_to_insert = None
+
+        while current is not None:
+            pos_to_insert = current
+
+            if key(value) < key(current.value):
+                current = current.left
+            else:
+                current = current.right
+
+        to_insert.parent = pos_to_insert
+
+        if pos_to_insert is None:
+            self.root = to_insert
+        elif key(value) < key(pos_to_insert.value):
+            pos_to_insert.left = to_insert
+        else:
+            pos_to_insert.right = to_insert
+
+        return to_insert
+
+    def find(self, value, key):
+        current = self.root
+
+        while current is not None and key(value) != key(current.value):
+            if key(value) < key(current.value):
+                current = current.left
+            else:
+                current = current.right
+
+        return current
+
+    def remove(self, node):
+        if node is None:
+            return
+
+        if node.left is None:
+            self.__transplant(node, node.right)
+        elif node.right is None:
+            self.__transplant(node, node.left)
+        else:
+            y = self.__leftmost(node.right)
+
+            if y.parent != node:
+                self.__transplant(y, y.right)
+                y.right = node.right
+                y.right.parent = y
+
+            self.__transplant(node, y)
+            y.left = node.left
+            y.left.parent = y
+
+    def __transplant(self, first, second):
+        if first.parent is None:
+            self.root = second
+        elif first == first.parent.left:
+            first.parent.left = second
+        else:
+            first.parent.right = second
+
+        if second is not None:
+            second.parent = first.parent
+
+    def __leftmost(self, node):
+        current = node
+
+        while current.left is not None and not current.is_leaf():
+            current = current.left
+
+        return current
+
+    def successor(self, node):
+        if node.right is not None:
+            return self.__minimum(node.right)
+
+        current = node.parent
+
+        while current is not None and node == current.right:
+            node = current
+            current = current.parent
+
+        return current
+
+    def __minimum(self, node):
+        current = node
+
+        while current is not None:
+            current = current.left
+
+        return current
+
+    def predecessor(self, node):
+        if node.left is not None:
+            return self.__maximum(node.left)
+
+        current = node.parent
+
+        while current is not None and node == current.left:
+            node = current
+            current = current.parent
+
+        return current
+
+    def __maximum(self, node):
+        current = node
+
+        while current is not None:
+            current = current.right
+
+        return current
--- Lab_3/main.py
+++ Lab_3/main.py
@@ -5,6 +5,7 @@ import bisect
 import matplotlib.pyplot as plt
 import heapq
 
+from bstree import BSTree
 
 ZERO = Decimal()
 INF = Decimal("inf")
@@ -21,6 +22,9 @@ class Point:
     def __eq__(self, other):
         return self.x == other.x and self.y == other.y
 
+    def __hash__(self):
+        return hash((self.x, self.y))
+
     def __str__(self):
         return f"({self.x}; {self.y})"
 
@@ -32,6 +36,9 @@ class Segment:
 
     def __eq__(self, other):
         return self.begin == other.begin and self.end == other.end
+
+    def __hash__(self):
+        return hash((self.begin, self.end))
 
     def __str__(self):
         return f"{self.begin} -> {self.end}"
@@ -84,6 +91,9 @@ class Segment:
 class Utils:
     @staticmethod
     def find_intersection(first, second):
+        if first is None or second is None:
+            return None
+
         k_1, b_1 = first.to_line()
         k_2, b_2 = second.to_line()
 
@@ -116,157 +126,72 @@ class EventType(Enum):
 
 
 class Event:
-    def __init__(self, point, segment, event_type):
+    def __init__(self, point, segments, event_type):
         self.point = point
-        self.segment = segment
+        self.segments = segments
         self.event_type = event_type
+
+    def __eq__(self, other):
+        return (self.point, self.segments, self.event_type) \
+               == (other.point, other.segments, other.event_type)
 
     def __lt__(self, other):
         return self.point < other.point
 
-class Node:
-    def __init__(self, value, left=None, right=None, parent=None):
-        self.value = value
-        self.left = left
-        self.right = right
-        self.parent = parent
+
+class SegmentComparator:
+    def __init__(self, x=ZERO):
+        self.x = x
+
+    def key(self, segment):
+        return segment.value(self.x)
 
 
-class BSTree:
-    def __init__(self):
-        self.root = None
-
-    def insert(self, value, key):
-        to_insert = Node(value)
-        current = self.root
-        pos_to_insert = None
-
-        while current is not None:
-            pos_to_insert = current
-
-            if key(value) < key(current):
-                current = current.left
-            else:
-                current = current.right
-
-        to_insert.parent = pos_to_insert
-
-        if pos_to_insert is None:
-            self.root = to_insert
-        else:
-            if key(value) < key(pos_to_insert.value):
-                pos_to_insert.left = to_insert
-            else:
-                pos_to_insert.right = to_insert
-
-        return to_insert
-
-    def remove(self, node):
-
-
-    def __transplant(self, first, second):
-        if first.parent is None:
-            self.root = second
-        elif first == first.parent.left:
-            first.parent.left = second
-        else:
-            first.parent.right = second
-
-        second.parent = first.parent
-
-    def successor(self, node):
-        if node.right is not None:
-            return self.__minimum(node.right)
-
-        current = node.parent
-
-        while current is not None and node == current.right:
-            node = current
-            current = current.parent
-
-        return current
-
-    def __minimum(self, node):
-        current = node
-
-        while current is not None:
-            current = current.left
-
-        return current
-
-    def predecessor(self, node):
-        if node.left is not None:
-            return self.__maximum(node.left)
-
-        current = node.parent
-
-        while current is not None and node == current.left:
-            node = current
-            current = current.parent
-
-        return current
-
-    def __maximum(self, node):
-        current = node
-
-        while current is not None:
-            current = current.right
-
-        return current
-
-
-# TODO: impl it
 class StatusStructure:
-    # def __init__(self):
-    #     self.sorted_list = []
-    #
-    # def insert(self, elem, key=None):
-    #     bisect.insort(self.sorted_list, elem, key=key)
-    #
-    # def remove(self, elem):
-    #     idx = self.__binary_search(elem)
-    #
-    #     if idx == -1:
-    #         return False
-    #
-    #     self.sorted_list.pop(idx)
-    #
-    #     return True
-    #
-    # def next(self, elem):
-    #     return self.__binary_search(elem) + 1
-    #
-    # def prev(self, elem):
-    #     return self.__binary_search(elem) - 1
-    #
-    # def swap(self, lhs, rhs):
-    #     lhs_idx = self.__binary_search(lhs)
-    #
-    #     if lhs_idx == -1:
-    #         return
-    #
-    #     rhs_idx = self.__binary_search(rhs)
-    #
-    #     if rhs_idx == -1:
-    #         return
-    #
-    #     self.sorted_list[lhs_idx], self.sorted_list[lhs_idx] = rhs, lhs
-    #
-    # def __binary_search(self, elem, key):
-    #     low = 0
-    #     high = len(elem) - 1
-    #
-    #     while high - low > 1:
-    #         mid = (low + high) // 2
-    #
-    #         if self.sorted_list[mid] < elem:
-    #             low = mid + 1
-    #         elif self.sorted_list[mid] > elem:
-    #             high = mid - 1
-    #         else:
-    #             return mid
-    #
-    #     return -1
+    def __init__(self):
+        self.comparator = SegmentComparator()
+        self.tree = BSTree()
+        self.segments = dict()
+
+    def set_x(self, x):
+        self.comparator.x = x
+
+    def insert(self, segment):
+        inserted_node = self.tree.insert(segment, lambda e, c=self.comparator: c.key(e))
+        self.segments[segment] = inserted_node
+
+    def delete(self, segment):
+        node_to_delete = self.segments[segment]
+        self.tree.remove(node_to_delete)
+        self.segments.pop(segment)
+
+    def next(self, segment):
+        node = self.segments[segment]
+        next_node = self.tree.successor(node)
+
+        if next_node is None:
+            return None
+
+        return next_node.value
+
+    def prev(self, segment):
+        node = self.segments[segment]
+        prev_node = self.tree.predecessor(node)
+
+        if prev_node is None:
+            return None
+
+        return prev_node.value
+
+    def swap(self, lhs, rhs):
+        if lhs not in self.segments and rhs not in self.segments:
+            return
+
+        self.tree.remove(self.segments[lhs])
+        self.tree.remove(self.segments[rhs])
+
+        self.insert(rhs)  # we insert rhs first to keep order after intersection
+        self.insert(lhs)
 
 
 class IntersectionDeterminator:
@@ -279,8 +204,10 @@ class IntersectionDeterminator:
         self.__plot_segments()
         self.__init_event_queue()
         result = self.__find_intersections()
+        self.__plot_intersections(result)
+        self.__report(result)
 
-        # plt.show()
+        plt.show()
 
     def __plot_segments(self):
         for segment in self.segments:
@@ -289,39 +216,115 @@ class IntersectionDeterminator:
     def __init_event_queue(self):
         for segment in self.segments:
             begin, end = segment.get_endpoints()
-            self.event_queue.append(Event(begin, segment, EventType.BEGIN))
-            self.event_queue.append(Event(end, segment, EventType.END))
+            self.event_queue.append(Event(begin, (segment,), EventType.BEGIN))
+            self.event_queue.append(Event(end, (segment,), EventType.END))
 
         heapq.heapify(self.event_queue)
 
     def __find_intersections(self):
-        # TODO: impl it
-        # result = []
-        #
-        # while len(self.event_queue) != 0:
-        #     cur_event = heapq.heappop(self.event_queue)
-        #     cur_point = cur_event.point
-        #
-        #     if cur_event.event_type == EventType.BEGIN:
-        #         segment =
+        result = []
+        intersected_segments = []
 
+        while len(self.event_queue) != 0:
+            cur_event = heapq.heappop(self.event_queue)
+            cur_point = cur_event.point
+            self.status.set_x(cur_point.x)
+
+            if cur_event.event_type == EventType.BEGIN:
+                segment = cur_event.segments[0]
+                self.status.insert(segment)
+
+                segment_above = self.status.next(segment)
+                segment_below = self.status.prev(segment)
+
+                above_current = Utils.find_intersection(segment_above, segment)
+                below_current = Utils.find_intersection(segment_below, segment)
+
+                if above_current is not None:
+                    intersected_segments.append((segment, segment_above))
+
+                if below_current is not None:
+                    intersected_segments.append((segment_below, segment))
+
+            elif cur_event.event_type == EventType.END:
+                segment = cur_event.segments[0]
+                segment_above = self.status.next(segment)
+                segment_below = self.status.prev(segment)
+
+                above_below = Utils.find_intersection(segment_above, segment_below)
+
+                if above_below is not None and cur_point < above_below:
+                    intersected_segments.append((segment_below, segment_above))
+
+                self.status.delete(segment)
+
+            elif cur_event.event_type == EventType.INTERSECTION:
+                lhs, rhs = cur_event.segments
+
+                segment_above = self.status.next(lhs)
+                segment_below = self.status.prev(rhs)
+
+                above_rhs = Utils.find_intersection(segment_above, rhs)
+                below_lhs = Utils.find_intersection(segment_below, lhs)
+
+                if above_rhs is not None and cur_point < above_rhs:
+                    intersected_segments.append((segment_above, rhs))
+
+                if below_lhs is not None and cur_point < below_lhs:
+                    intersected_segments.append((lhs, segment_below))
+
+                self.status.swap(lhs, rhs)
+
+            else:
+                raise Exception("Unknown event")
+
+            while len(intersected_segments) != 0:
+                lhs, rhs = intersected_segments.pop()
+                intersection = Utils.find_intersection(lhs, rhs)
+                event = Event(intersection, (lhs, rhs), EventType.INTERSECTION)
+
+                if event not in self.event_queue:
+                    result.append(intersection)
+                    self.__add_event(event)
+
+        return result
+
+    def __add_event(self, event):
+        self.event_queue.append(event)
+        heapq.heapify(self.event_queue)
+
+    def __plot_intersections(self, intersections):
+        for point in intersections:
+            plt.scatter(point.x, point.y, color="red")
+
+    def __report(self, result):
+        print(f"Count: {len(result)}")
+        print("Intersections: ")
+
+        for point in result:
+            print(point)
+
+
+def create_segment(x_begin, y_begin, x_end, y_end):
+    return Segment(Point(Decimal(x_begin),
+                         Decimal(y_begin)),
+                   Point(Decimal(x_end),
+                         Decimal(y_end)))
 
 def main():
     segments = [
-        Segment(Point(Decimal(-2), Decimal(0)), Point(Decimal(2), Decimal(1))),
-        Segment(Point(Decimal(0), Decimal(0)), Point(Decimal(0), Decimal(2)))
+        create_segment(-3, 0, 2, 1),
+        create_segment(1, 5, 3, 3),
+        create_segment(0, 5, 2, 0),
+        create_segment(1, 2, 2, 7),
+        create_segment(-2, 1, 3, 0),
+        create_segment(3, 5, -3, -1),
+        create_segment(-0.5, -1, 0, 3),
+        create_segment(0, 4, 1, 7)
     ]
-
-    for s in segments:
-        print(s)
 
     intersection_determinator = IntersectionDeterminator(segments)
     intersection_determinator.demo()
-
-    intersection = Utils.find_intersection(segments[0], segments[1])
-    # if intersection is not None:
-    #     plt.scatter(intersection.x, intersection.y, color="red")
-    #     print(intersection)
 
     plt.show()
 
